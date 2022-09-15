0eb7dd12c305f3e2cda14a43095f6a0f2d6deb63
Ben Stautner <benjamin.stautner@ars.de>
2022-09-15 14:35:30 +0200

    Lifecycle hooks und State Changes (Angular 14)

--- src/app/todo-control-panel/todo-control-panel.component.ts
+++ src/app/todo-control-panel/todo-control-panel.component.ts
@@ -1,15 +1,51 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'todo-todo-control-panel',
   templateUrl: './todo-control-panel.component.html',
   styleUrls: ['./todo-control-panel.component.css']
 })
-export class TodoControlPanelComponent {
+export class TodoControlPanelComponent implements DoCheck, OnInit, OnChanges, OnDestroy {
 
   @Input() public todoCount: number | undefined;
   @Input() public displayAllEmails: boolean = false;
   @Output() public displayAllEmailsChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  public ngOnInit(): void {
+    console.log('Panel initialisiert');
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    for (const propertyName in changes) {
+      if (changes.hasOwnProperty(propertyName)) {
+        const change = changes[propertyName];
+
+        console.log(
+          `${ propertyName } hat sich von ${ change.previousValue } zu ${
+            change.currentValue
+          } geändert`
+        );
+      }
+    }
+  }
+
+  public ngDoCheck(): void {
+    console.log('Panel wird auf Änderungen geprüft');
+  }
+
+  public ngOnDestroy(): void {
+    console.log('Panel zerstört');
+  }
 
   public toggleShowEmail(): void {
     this.displayAllEmails = !this.displayAllEmails;
