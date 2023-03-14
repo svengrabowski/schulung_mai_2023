f26659dda6e0ee88c1aecc0d9165ae390a671502
Ben Stautner <benjamin.stautner@ars.de>
2023-03-14 15:15:07 +0100

    add missing files

---
+++ src/app/shared/todo-item.ts
@@ -0,0 +1,11 @@
+export class TodoItem {
+  constructor(
+    public title = '',
+    public description = '',
+    public email = '',
+    public completed = false,
+    public id?: number,
+    public group = ''
+  ) {
+  }
+}
